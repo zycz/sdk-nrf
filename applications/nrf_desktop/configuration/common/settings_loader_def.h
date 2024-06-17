8e8c2205af0f89a7bcdc916ec630c89261a64088
Jan Zyczkowski <jan.zyczkowski@nordicsemi.no>
2024-06-17 13:26:54 +0200

    Disable not BLE modules and features of nrfDesktop for smp bug debugging

--- applications/nrf_desktop/configuration/common/settings_loader_def.h
+++ applications/nrf_desktop/configuration/common/settings_loader_def.h
@@ -20,22 +20,22 @@ const struct {} settings_loader_def_include_once;
 static inline void get_req_modules(struct module_flags *mf)
 {
 	module_flags_set_bit(mf, MODULE_IDX(main));
-#if CONFIG_DESKTOP_HIDS_ENABLE
-	module_flags_set_bit(mf, MODULE_IDX(hids));
-#endif
-#if CONFIG_DESKTOP_BAS_ENABLE
-	module_flags_set_bit(mf, MODULE_IDX(bas));
-#endif
+// #if CONFIG_DESKTOP_HIDS_ENABLE
+// 	module_flags_set_bit(mf, MODULE_IDX(hids));
+// #endif
+// #if CONFIG_DESKTOP_BAS_ENABLE
+// 	module_flags_set_bit(mf, MODULE_IDX(bas));
+// #endif
 #ifdef CONFIG_CAF_BLE_STATE
 	module_flags_set_bit(mf, MODULE_IDX(ble_state));
 #endif
-#if CONFIG_DESKTOP_MOTION_SENSOR_ENABLE
-	module_flags_set_bit(mf, MODULE_IDX(motion));
-#endif
+// #if CONFIG_DESKTOP_MOTION_SENSOR_ENABLE
+// 	module_flags_set_bit(mf, MODULE_IDX(motion));
+// #endif
 #if CONFIG_DESKTOP_BLE_SCAN_ENABLE
 	module_flags_set_bit(mf, MODULE_IDX(ble_scan));
 #endif
-#if CONFIG_DESKTOP_FAILSAFE_ENABLE
-	module_flags_set_bit(mf, MODULE_IDX(failsafe));
-#endif
+// #if CONFIG_DESKTOP_FAILSAFE_ENABLE
+// 	module_flags_set_bit(mf, MODULE_IDX(failsafe));
+// #endif
 }
