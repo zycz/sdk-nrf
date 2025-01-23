c19607de407c793b12e4ad337346f862938862ab
Jan Zyczkowski <jan.zyczkowski@nordicsemi.no>
2025-01-23 12:58:11 +0100

    idle sample test radio core extra power active usage

--- tests/benchmarks/multicore/idle/remote/src/main.c
+++ tests/benchmarks/multicore/idle/remote/src/main.c
@@ -9,24 +9,30 @@
 #include <zephyr/logging/log.h>
 #include <soc_lrcconf.h>
 
-extern sys_snode_t soc_node;
-
 LOG_MODULE_REGISTER(idle);
+
+extern sys_snode_t soc_node;
 
 int main(void)
 {
-	k_sleep(K_FOREVER);
-	// soc_lrcconf_poweron_release(&soc_node, NRF_LRCCONF_POWER_DOMAIN_0);
+	// k_sleep(K_FOREVER);
 // 	unsigned int cnt = 0;
 
 // #if defined CONFIG_FIRST_SLEEP_OFFSET
 // 	k_msleep(1000);
 // #endif
-
+	soc_lrcconf_poweron_release(&soc_node, NRF_LRCCONF_POWER_DOMAIN_0);
 	// LOG_INF("Multicore idle test on %s", CONFIG_BOARD_TARGET);
+
+	// nrf_lrcconf_poweron_force_set(NRF_LRCCONF010, NRF_LRCCONF_POWER_MAIN, true);
+	// NRF_LRCCONF010->EVENTS_HFXOSTARTED = 0x0;
+	// NRF_LRCCONF010->TASKS_REQHFXO = 0x1;
+	int64_t next_timeout = k_uptime_get();
 	while (1) {
 		// LOG_INF("Multicore idle test iteration %u", cnt++);
-		k_msleep(1);
+		next_timeout += 10;
+		k_sleep(K_TIMEOUT_ABS_MS(next_timeout));
+
 	}
 
 	return 0;
