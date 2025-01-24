cf2986895688e84be5802f97a8f4b7514a642c67
Jan Zyczkowski <jan.zyczkowski@nordicsemi.no>
2025-01-24 18:44:32 +0100

    add gpio toggle

--- applications/nrf_desktop/src/main.c
+++ applications/nrf_desktop/src/main.c
@@ -11,10 +11,21 @@
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(MODULE);
+#include <hal/nrf_gpio.h>
 
+static void gpio_init(void)
+{
+	nrf_gpio_cfg_output(NRF_GPIO_PIN_MAP(1, 3));
+	nrf_gpio_pin_clear(NRF_GPIO_PIN_MAP(1, 3));
+	nrf_gpio_cfg_output(NRF_GPIO_PIN_MAP(1, 4));
+	nrf_gpio_pin_clear(NRF_GPIO_PIN_MAP(1, 4));
+	nrf_gpio_cfg_output(NRF_GPIO_PIN_MAP(1, 5));
+	nrf_gpio_pin_clear(NRF_GPIO_PIN_MAP(1, 5));
+}
 
 int main(void)
 {
+	gpio_init();
 	if (app_event_manager_init()) {
 		LOG_ERR("Application Event Manager not initialized");
 	} else {
