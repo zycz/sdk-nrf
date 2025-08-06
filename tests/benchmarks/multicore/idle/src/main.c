b919b8dcdf0e90903e1caeb89794182147dc3ad3
Jan Zyczkowski <jan.zyczkowski@nordicsemi.no>
2025-08-06 15:05:23 +0200

    Change DVFS frequency to 64MHz

--- tests/benchmarks/multicore/idle/src/main.c
+++ tests/benchmarks/multicore/idle/src/main.c
@@ -8,6 +8,8 @@
 #include <zephyr/sys/printk.h>
 #include <zephyr/logging/log.h>
 
+#include "ld_dvfs_handler.h"
+
 LOG_MODULE_REGISTER(idle);
 
 int main(void)
@@ -17,6 +19,18 @@ int main(void)
 #ifdef CONFIG_FIRST_SLEEP_OFFSET
 	k_msleep(1000);
 #endif
+
+	int status = dvfs_service_handler_change_freq_setting(DVFS_FREQ_LOW);
+
+	if (status == -EAGAIN) {
+		LOG_WRN("DVFS not initialized, try again.");
+	} else if (status == -EBUSY) {
+		LOG_WRN("DVFS frequency change in progress");
+	} else if (status != 0) {
+		LOG_ERR("DVFS freq change returned with error: %d", status);
+	} else {
+		LOG_INF("Requesting frequency setting DVFS_FREQ_LOW");
+	}
 
 	LOG_INF("Multicore idle test on %s", CONFIG_BOARD_TARGET);
 	while (1) {
